58bb6cf175ad9ff6ca68549af62dd2ca271c6db7
poq222 <wowwow314159@gmail.com>
2024-12-12 10:54:34 +0800

    ,

--- entrance/data.js
+++ entrance/data.js
@@ -268,6 +268,6 @@ var APP_DATA = {
     "mouseViewMode": "drag",
     "autorotateEnabled": true,
     "fullscreenButton": true,
-    "viewControlButtons": true
+    "viewControlButtons": true,
   }
 };
