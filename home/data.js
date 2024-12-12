98ab95c9c625ba60f102a1fe198c80139f626d34
poq222 <wowwow314159@gmail.com>
2024-12-12 11:13:07 +0800

    ..

---
+++ home/data.js
@@ -0,0 +1,42 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0--",
+      "name": "清水寺"
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1680,
+      "initialViewParameters": {
+        "yaw": 0.7606356146676685,
+        "pitch": -0.0007492097747796578,
+        "fov": 0.7418628213809836
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "清水寺720環景",
+  "settings": {
+    "mouseViewMode": "drag",
+  "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
